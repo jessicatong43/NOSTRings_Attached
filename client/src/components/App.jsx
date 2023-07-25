a592f401b136f936edafcdfa6be8b29d6ca7834b
NicMilli <nicbass2@gmail.com>
2023-07-25 13:04:20 -0700

    React router added

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import '../index.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 
-export default function App() {
-
+function App() {
   return (
-    <div id="App">
-      Hello world
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<div>Hello World</div>} />
+          {/* <Route path='/' element={<Explore/>} />
+          <Route path='/profile' element={<PrivateRoute />} >
+            <Route path='/profile' element={<Profile/>} />
+          </Route>
+          <Route path='/sign-in' element={<SignIn/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+          <Route path='/forgot-password' element={<ForgotPassword/>} />
+          <Route path='/create-newsletter' element={<CreateListing/>} />
+          <Route path='/edit-edition/:editionId' element={<EditListing/>} /> */}
+        </Routes>
+        <Navbar/>
+      </Router>
+
+      <ToastContainer />
+    </>
   );
 }
+
+export default App;
