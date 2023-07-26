9187742a5b1bcef76c047edca470b19c4ef23af7
NicMilli <nicbass2@gmail.com>
2023-07-25 19:45:40 -0700

    index.html changed to display bundle.js

---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase.config';
+
+function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const { email, password, username } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+      const { user } = userCredential;
+
+      updateProfile(auth.currentUser, {
+        displayName: username,
+      });
+
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy);
+
+      navigate('/');
+
+      toast.success(`Welcome ${username}`);
+    } catch (error) {
+      console.log(error);
+      toast.error('Registration error: please use a valid email and more than 8 characters in the password!');
+    }
+  };
+
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">
+          Sign Up
+        </p>
+      </header>
+
+      <main>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            className="nameInput"
+            placeholder="Username"
+            id="username"
+            value={username}
+            onChange={onChange}
+          />
+
+          <input
+            type="email"
+            className="emailInput"
+            placeholder="Email"
+            id="email"
+            value={email}
+            onChange={onChange}
+          />
+
+          <div className="passwordInputDiv">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              className="passwordInput"
+              placeholder="Password"
+              id="password"
+              value={password}
+              onChange={onChange}
+            />
+
+            <img
+              src={visibilityIcon}
+              alt="show password"
+              className="showPassword"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+
+          <Link to="/forgot-password">
+            Forgot Password
+          </Link>
+
+          <div>
+            <p>
+              Sign Up
+            </p>
+            <button>
+              <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+
+        <Link to="/sign-in">
+          Sign In Instead
+        </Link>
+      </main>
+    </div>
+  );
+}
+
+export default SignUp;
